e0d2e0f710c10a11de7fef20838598714215be36
bo.johnson <bo@letterxdesign.com>
2014-07-25 16:10:51 -0800

    Switch social domain to ageomatic.co

--- footer.php
+++ footer.php
@@ -18,14 +18,14 @@
 				  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=257163947752145&version=v2.0";
 				  fjs.parentNode.insertBefore(js, fjs);
 				}(document, 'script', 'facebook-jssdk'));</script>
-				<div class="fb-like" data-href="http://www.letterxdesign.com/apex/ageomatic" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+				<div class="fb-like" data-href="http://ageomatic.co" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
 			</div>
 			<div id="tell">Want to Share?</div>
 			<div id="twitter">
 				<?php if($_GET) { ?>
-					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.letterxdesign.com/apex/ageomatic" data-text="#ageomatic tells me <?php echo $name; ?> is <?php echo $bday->age($now, $in, $secsIn); ?> old." data-via="ageomatic">Tweet</a>
+					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://ageomatic.co" data-text="#ageomatic tells me <?php echo $name; ?> is <?php echo $bday->age($now, $in, $secsIn); ?> old." data-via="ageomatic">Tweet</a>
 				<?php } else { ?>
-					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.letterxdesign.com/apex/ageomatic" data-text="Find out how old you are with the #ageomatic" data-via="ageomatic">Tweet</a>
+					<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://ageomatic.co" data-text="Find out how old you are with the #ageomatic" data-via="ageomatic">Tweet</a>
 				<?php } ?>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 			</div>
